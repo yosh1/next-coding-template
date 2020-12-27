a305da834a9f396259124948ee0f4f0d0f2a8952
yosh1 <yoppy1125yoppy@gmail.com>
2020-12-28 00:47:18 +0900

    Add pages

---
+++ pages/head.js
@@ -0,0 +1,63 @@
+import Head from "next/head";
+
+export default function Heads() {
+  const title = "",
+    description = "",
+    image = "",
+    url = "",
+    twitter = "",
+    color = "#000000";
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="blog" />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+      <meta property="og:site_name" content={title} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={twitter} />
+      <meta name="twitter:url" content={image} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <link rel="canonical" href={url} />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content={color} />
+      <meta name="theme-color" content="#ffffff" />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=XXXXXXXXXXXX"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments)}
+        gtag("js", new Date());
+        gtag("config", "XXXXXXXXX");
+    `,
+        }}
+      ></script>
+    </Head>
+  );
+}
